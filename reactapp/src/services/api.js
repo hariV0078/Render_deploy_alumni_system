500649e31a646425304187b1211bd48623b316cf
HARI VARADHAN <hshri511@gmail.com>
2025-08-25 11:47:14 +0530

    Changes in baseurl

--- reactapp/src/services/api.js
+++ reactapp/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://render-deploy-alumni-system-1.onrender.com/api",
 });
 
 // Attach token if present
